76757e03bef6dec9e327071b39170ca262791e48
Julie Altman <jtskolnik@gmail.com>
2020-09-26 16:05:44 -0400

    Removed the parameters from the function definition as they will not change while the page is active

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 //Prompts user to enter the length of the password
 var pwrdLength = prompt("How many characters?");
 pwrdLength = parseInt(pwrdLength);
@@ -98,14 +83,31 @@ if(numYes == true){
 }
 else{}
 
-randomStr(pwrdLength,userCharacters);
-//This generates a variable called anslwr that is the password
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-function randomStr(len, arr) { 
-  var anslwr = ''; 
-  for (var i = len; i > 0; i--) { 
-      anslwr +=  
-        arr[Math.floor(Math.random() * arr.length)]; 
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+//This generates a variable called anslwr that is the password
+var password = randomStr();
+
+function randomStr() { 
+  var genPasswd = ''; 
+  for (var i = pwrdLength; i > 0; i--) { 
+      genPasswd +=  
+        userCharacters[Math.floor(Math.random() * userCharacters.length)]; 
   } 
-  console.log(anslwr);
+  console.log(genPasswd);
 } 
